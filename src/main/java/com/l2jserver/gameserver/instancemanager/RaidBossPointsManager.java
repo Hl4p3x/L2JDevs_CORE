01d85242a9d3ef8121c7cb40a3425be082d6ef51
Sacrifice <sacrifice@engineer.com>
2018-11-01 13:35:01 +0100

    Fix for Character Raid Points
    - It seems that for some strange reason the points that the players
    obtain by killing raids were not added to the database or for some
    reason it seems that they were in the inserted database, but after
    restarting the server or by a sudden shutdown the data in the end they
    were not really inserted. It has been used to rework the class to adapt
    it to the future.

--- src/main/java/com/l2jserver/gameserver/instancemanager/RaidBossPointsManager.java
+++ src/main/java/com/l2jserver/gameserver/instancemanager/RaidBossPointsManager.java
@@ -22,7 +22,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -30,79 +29,70 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.l2jserver.commons.database.pool.impl.ConnectionFactory;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 
 /**
- * @author Kerberos, JIV
- * @version 8/24/10
+ * @author Kerberos, JIV, Sacrifice
  */
 public class RaidBossPointsManager
 {
-	private static final Logger _log = Logger.getLogger(RaidBossPointsManager.class.getName());
+	private static final Logger LOG = LoggerFactory.getLogger(RaidBossPointsManager.class);
 	
-	private final Map<Integer, Map<Integer, Integer>> _list = new ConcurrentHashMap<>();
+	private static final String SELECT = "SELECT charId, boss_id, points FROM character_raid_points";
+	private static final String REPLACE = "REPLACE INTO character_raid_points (charId, boss_id, points) VALUES (?, ?, ?)";
+	private static final String DELETE = "DELETE from character_raid_points WHERE charId > 0";
+	
+	private final Map<Integer, Map<Integer, Integer>> list = new ConcurrentHashMap<>();
 	
 	public RaidBossPointsManager()
 	{
 		init();
 	}
 	
-	private final void init()
+	public final void addPoints(L2PcInstance player, int bossId, int points)
 	{
-		try (Connection con = ConnectionFactory.getInstance().getConnection();
-			Statement s = con.createStatement();
-			ResultSet rs = s.executeQuery("SELECT `charId`,`boss_id`,`points` FROM `character_raid_points`"))
-		{
-			while (rs.next())
-			{
-				int charId = rs.getInt("charId");
-				int bossId = rs.getInt("boss_id");
-				int points = rs.getInt("points");
-				Map<Integer, Integer> values = _list.get(charId);
-				if (values == null)
-				{
-					values = new HashMap<>();
-				}
-				values.put(bossId, points);
-				_list.put(charId, values);
-			}
-			_log.info(getClass().getSimpleName() + ": Loaded " + _list.size() + " Characters Raid Points.");
-		}
-		catch (SQLException e)
-		{
-			_log.log(Level.WARNING, getClass().getSimpleName() + ": Couldnt load raid points ", e);
-		}
+		final Map<Integer, Integer> tmpPoint = list.computeIfAbsent(player.getObjectId(), k -> new HashMap<>());
+		updatePointsInDB(player, bossId, tmpPoint.merge(bossId, points, Integer::sum));
 	}
 	
-	public final void updatePointsInDB(L2PcInstance player, int raidId, int points)
+	public final int calculateRanking(int playerObjId)
+	{
+		final Map<Integer, Integer> rank = getRankList();
+		if (rank.containsKey(playerObjId))
+		{
+			return rank.get(playerObjId);
+		}
+		return 0;
+	}
+	
+	public final void cleanUp()
 	{
 		try (Connection con = ConnectionFactory.getInstance().getConnection();
-			PreparedStatement ps = con.prepareStatement("REPLACE INTO character_raid_points (`charId`,`boss_id`,`points`) VALUES (?,?,?)"))
+			PreparedStatement ps = con.prepareStatement(DELETE))
 		{
-			ps.setInt(1, player.getObjectId());
-			ps.setInt(2, raidId);
-			ps.setInt(3, points);
 			ps.executeUpdate();
+			list.clear();
+			ps.close();
 		}
 		catch (Exception e)
 		{
-			_log.log(Level.WARNING, getClass().getSimpleName() + ": Couldn't update char raid points for player: " + player, e);
+			LOG.warn("{}: Couldn't clean raid points. {}", getClass().getSimpleName(), e);
 		}
 	}
 	
-	public final void addPoints(L2PcInstance player, int bossId, int points)
+	public final Map<Integer, Integer> getList(L2PcInstance player)
 	{
-		final Map<Integer, Integer> tmpPoint = _list.computeIfAbsent(player.getObjectId(), k -> new HashMap<>());
-		updatePointsInDB(player, bossId, tmpPoint.merge(bossId, points, Integer::sum));
+		return list.get(player.getObjectId());
 	}
 	
 	public final int getPointsByOwnerId(int ownerId)
 	{
-		Map<Integer, Integer> tmpPoint = _list.get(ownerId);
+		final Map<Integer, Integer> tmpPoint = list.get(ownerId);
 		int totalPoints = 0;
 		
 		if ((tmpPoint == null) || tmpPoint.isEmpty())
@@ -117,56 +107,74 @@ public class RaidBossPointsManager
 		return totalPoints;
 	}
 	
-	public final Map<Integer, Integer> getList(L2PcInstance player)
-	{
-		return _list.get(player.getObjectId());
-	}
-	
-	public final void cleanUp()
-	{
-		try (Connection con = ConnectionFactory.getInstance().getConnection();
-			Statement s = con.createStatement())
-		{
-			s.executeUpdate("DELETE from character_raid_points WHERE charId > 0");
-			_list.clear();
-		}
-		catch (Exception e)
-		{
-			_log.log(Level.WARNING, getClass().getSimpleName() + ": Couldn't clean raid points", e);
-		}
-	}
-	
-	public final int calculateRanking(int playerObjId)
-	{
-		Map<Integer, Integer> rank = getRankList();
-		if (rank.containsKey(playerObjId))
-		{
-			return rank.get(playerObjId);
-		}
-		return 0;
-	}
-	
 	public Map<Integer, Integer> getRankList()
 	{
 		final Map<Integer, Integer> tmpPoints = new HashMap<>();
-		for (int ownerId : _list.keySet())
+		for (int ownerId : list.keySet())
 		{
-			int totalPoints = getPointsByOwnerId(ownerId);
+			final int totalPoints = getPointsByOwnerId(ownerId);
 			if (totalPoints != 0)
 			{
 				tmpPoints.put(ownerId, totalPoints);
 			}
 		}
 		
-		final List<Entry<Integer, Integer>> list = new ArrayList<>(tmpPoints.entrySet());
-		list.sort(Comparator.comparing(Entry<Integer, Integer>::getValue).reversed());
+		final List<Entry<Integer, Integer>> listRanking = new ArrayList<>(tmpPoints.entrySet());
+		listRanking.sort(Comparator.comparing(Entry<Integer, Integer>::getValue).reversed());
 		int ranking = 1;
 		final Map<Integer, Integer> tmpRanking = new HashMap<>();
-		for (Entry<Integer, Integer> entry : list)
+		for (Entry<Integer, Integer> entry : listRanking)
 		{
 			tmpRanking.put(entry.getKey(), ranking++);
 		}
 		return tmpRanking;
+	}
+	
+	public final void updatePointsInDB(L2PcInstance player, int raidId, int points)
+	{
+		try (Connection con = ConnectionFactory.getInstance().getConnection();
+			PreparedStatement ps = con.prepareStatement(REPLACE))
+		{
+			ps.setInt(1, player.getObjectId());
+			ps.setInt(2, raidId);
+			ps.setInt(3, points);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (Exception e)
+		{
+			LOG.warn("{}: Couldn't update char raid points for player: {} {}", getClass().getSimpleName(), player, e);
+		}
+	}
+	
+	private final void init()
+	{
+		try (Connection con = ConnectionFactory.getInstance().getConnection();
+			PreparedStatement ps = con.prepareStatement(SELECT);
+			ResultSet rset = ps.executeQuery())
+		{
+			while (rset.next())
+			{
+				final int charId = rset.getInt("charId");
+				final int bossId = rset.getInt("boss_id");
+				final int points = rset.getInt("points");
+				Map<Integer, Integer> values = list.get(charId);
+				
+				if (values == null)
+				{
+					values = new HashMap<>();
+				}
+				values.put(bossId, points);
+				list.put(charId, values);
+			}
+			LOG.info("{}: Loaded {} Character Raid Points.", getClass().getSimpleName(), list.size());
+			rset.close();
+			ps.close();
+		}
+		catch (SQLException e)
+		{
+			LOG.warn("{}: Couldn't load raid points {}", getClass().getSimpleName(), e);
+		}
 	}
 	
 	public static final RaidBossPointsManager getInstance()
