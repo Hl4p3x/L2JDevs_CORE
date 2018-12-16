fc16c2b99268a4679716e022300ad0e937b47cf6
Sacrifice <sacrifice@engineer.com>
2018-12-16 21:23:24 +0000

    Revert "Merge branch 'fix-sit-get-up' into 'master'"
    This reverts merge request !24

--- src/main/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
+++ src/main/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
@@ -4663,6 +4663,14 @@ public final class L2PcInstance extends L2Playable
 	@Override
 	public void doCast(Skill skill)
 	{
+		if (getCurrentSkill() != null)
+		{
+			if (!checkUseMagicConditions(skill, getCurrentSkill().isCtrlPressed(), getCurrentSkill().isShiftPressed()))
+			{
+				setIsCastingNow(false);
+				return;
+			}
+		}
 		super.doCast(skill);
 		setRecentFakeDeath(false);
 	}
@@ -7517,11 +7525,6 @@ public final class L2PcInstance extends L2Playable
 			// just ignore the passive skill request. why does the client send it anyway ??
 			// Send a Server->Client packet ActionFailed to the L2PcInstance
 			sendPacket(ActionFailed.STATIC_PACKET);
-			return false;
-		}
-		
-		if (!checkUseMagicConditions(skill, forceUse, dontMove))
-		{
 			return false;
 		}
 		
