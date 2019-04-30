65c7a10d77160b79f32a25027c0b8969ce480929
U3Games <u3.swarlog@gmail.com>
2019-04-30 20:03:59 +0200

    Add database installer for gs and ls.

---
+++ dist/tools/dbinst_gs.bat
@@ -0,0 +1,5 @@
+@echo off
+title Game Server DataBase Installer
+color 17
+java -jar dbinst_gs.jar
+pause
---
+++ dist/tools/dbinst_ls.bat
@@ -0,0 +1,5 @@
+@echo off
+title Login Server DataBase Installer
+color 17
+java -jar dbinst_ls.jar
+pause
